8aecf64986faab07b5b1c1fe291ca104a9f3f715
MadhusudhanDama <52232163+MadhusudhanDama@users.noreply.github.com>
2020-08-20 11:35:44 +0530

    port number changed

---
+++ CustomerWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhu.io.customerwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
