0d4174df3746b5cebb76d5f1d7507123932d6f03
MadhusudhanDama <52232163+MadhusudhanDama@users.noreply.github.com>
2020-08-20 11:35:44 +0530

    Add files via upload

---
+++ CustomerController.java
@@ -0,0 +1,15 @@
+package com.madhu.io.customerwebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomerController {
+	
+	@RequestMapping("/")
+	public String index(Model model) {
+		return "index";
+	}
+
+}
